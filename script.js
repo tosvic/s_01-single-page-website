4fa05c1937d77bf745decca513f43c7651f4ddbb
tosvic <victoruhuo@yahoo.com>
2024-07-02 22:14:47 +0100

    foot

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
+// currentDay
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 const wkday = new Date();
 let day = weekday[wkday.getUTCDay()];
 document.getElementById("day").innerHTML = day;
 
 
-
-const time = new Time();
-document.getElementById("time").innerHTML = time;
+// currentTimeUTC
+const currentTime = new Date();
+document.getElementById("time").innerHTML = currentTime.getHours() + ":" + currentTime.getMinutes();
