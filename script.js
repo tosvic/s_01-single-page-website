e2ee36b10374b345288fbf597c67bca38bd13bbc
tosvic <victoruhuo@yahoo.com>
2024-07-02 21:11:50 +0100

    portfo

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const wkday = new Date();
+let day = weekday[wkday.getUTCDay()];
+document.getElementById("day").innerHTML = day;
+
+
+
+const time = new Time();
+document.getElementById("time").innerHTML = time;
